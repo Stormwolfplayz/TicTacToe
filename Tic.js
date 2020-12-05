a6b3078caa0c93902b33a0db7d1205e42087e0b6
Stormwolfplayz <36309142+Stormwolfplayz@users.noreply.github.com>
2020-12-05 17:52:17 -0600

    Create Tic.js

---
+++ Tic.js
@@ -0,0 +1,57 @@
+var btns = [];
+var lol = "test";
+var turn = "X"
+var t = [0,0,0,0,0,0,0,0,0];
+
+for (i = 0; i < 10; i++) {
+  lol = i.toString();
+  btns.push(document.getElementById(i));
+}
+
+
+
+
+for (let i = 0; i < 9; i++) {
+  btns[i].addEventListener("mousedown", function() {
+   
+
+    if (turn == "X") {
+      t[i] = turn;
+      btns[i].innerHTML = "X";
+      turn = "O";
+      
+    } else if (turn == "O") {
+      btns[i].innerHTML = "O";
+      turn = "X";
+    }
+    
+    
+    
+  });
+}
+function test(){
+  
+  function allEqual(arr) {
+    return new Set(arr).size == 1;
+  }
+  console.log(t[4]+", "+t[5]+", "+t[6])
+   // true
+  if (t[1] != "0" && allEqual([t[0], t[1], t[2]]) == true) {
+    console.log(t[1] + " is the winner!")
+    }
+    else if (t[4] != "0" && allEqual([t[4], t[5], t[6]]) == true) {
+    console.log(t[4] + " is the winner!")
+    }
+    else if (t[7] != "0" && allEqual([t[7], t[8], t[9]]) == true) {
+    console.log(t[7] + " is the winner!")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
