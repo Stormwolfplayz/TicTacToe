26786d1b6af1b807e75045f4c045a06cb3974784
Stormwolfplayz <36309142+Stormwolfplayz@users.noreply.github.com>
2020-12-05 23:26:02 -0600

    Update Tic.js

--- Tic.js
+++ Tic.js
@@ -1,56 +1,61 @@
-var btns = [];
-var lol = "test";
-var turn = "X"
-var t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-for (i = 0; i < 10; i++) {
-  lol = i.toString();
-  btns.push(document.getElementById(i));
-}
-
-
-
-
-for (let i = 0; i < 9; i++) {
-  btns[i].addEventListener("mousedown", function() {
+window.onload = function(){
+  
+    var btns = [];
+    var lol = "test";
+    var turn = "X"
+    var t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var ex = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (i = 0; i < 10; i++) {
+      lol = i.toString();
+      btns.push(document.getElementById(i));
+    }
 
 
-    if (turn == "X") {
-      t[i] = turn;
-      btns[i].innerHTML = "X";
-      turn = "O";
+		
 
-    } else if (turn == "O") {
-      btns[i].innerHTML = "O";
-      turn = "X";
+    for (let i = 0; i < 9; i++) {
+      btns[i].addEventListener("mousedown", function() {
+        t[i] = turn;
+
+        if (turn == "X") {
+
+          btns[i].innerHTML = "X";
+          turn = "O";
+
+        } else if (turn == "O") {
+          t[i] =
+            btns[i].innerHTML = "O";
+          turn = "X";
+        }
+
+
+
+      });
+    }
+
+		function reStart(){
+    	for (i=0; i<9; i++){
+      	btns[i].disabled=false;
+        btns[i].innerHTML="​";
+      }
     }
 
 
 
-  });
-}
-
-function test() {
-
-  function allEqual(arr) {
-    return new Set(arr).size == 1;
-  }
- 
-  // true
-  if (t[1] != "0" && allEqual([t[0], t[1], t[2]]) == true) {
-    console.log(t[1] + " is the winner!")
-  } else if (t[4] != "0" && allEqual([t[3], t[4], t[5]]) == true) {
-    console.log(t[4] + " is the winner!")
-  } else if (t[7] != "0" && allEqual([t[6], t[7], t[8]]) == true) {
-    console.log(t[7] + " is the winner!")
-  }
 
 
+    function win(ex) {
+
+      for (var i = 0; i < t.length; i++) {
+        if (t[i] !== ex[i]) return false;
+      }
+      return true;
+    }
+
+    function run() {
+      console.log(win('X', 0, 0, 0, 'X', 0, 0, 0, 'X'))
 
 
-
-
-
-
-
+    }
+  
 }
